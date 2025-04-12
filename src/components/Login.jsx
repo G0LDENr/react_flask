6a2bde257fcb83f8916f67f9094f7df5efa791b5
GOLDENr <carlos.villavicenciog180@gmail.com>
2025-04-11 23:58:10 -0600

    actualizacion de api

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
     setError('');
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/user/login', {
+      const response = await fetch('http://18.222.162.217/user/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
